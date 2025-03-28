83a7200734b2869f098f605c63858e1b6cca465c
sherif.abdelbast@gmail.com <sherifabdelbast>
2025-03-29 00:36:36 +0200

    create watch list component

---
+++ src/app/watch-list/watch-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watch-list',
+  imports: [],
+  templateUrl: './watch-list.component.html',
+  styleUrl: './watch-list.component.css'
+})
+export class WatchListComponent {
+
+}
