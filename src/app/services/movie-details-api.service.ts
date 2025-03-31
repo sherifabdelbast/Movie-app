091c5192cd0113f399c30cce1a02365aaebff1d1
Amro Hesham <mr487680@gmail.com>
2025-03-31 16:31:54 +0200

    added the api links

--- src/app/services/movie-details-api.service.ts
+++ src/app/services/movie-details-api.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieDetailsApiService {
+  http = inject(HttpClient);
 
-  constructor() { }
+  getMovieDetails(id: string): Observable<any> {
+    return this.http.get(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=2fcc1a253fc8e132b7699c023f783b1f`
+    );
+  }
+
+  getRecommendations(id: string): Observable<any> {
+    return this.http.get(
+      `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=2fcc1a253fc8e132b7699c023f783b1f`
+    );
+  }
+
+  getReviews(id: string): Observable<any> {
+    return this.http.get(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=2fcc1a253fc8e132b7699c023f783b1f`
+    );
+  }
+
+  constructor() {}
 }
