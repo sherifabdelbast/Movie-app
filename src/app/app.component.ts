2969072ee79f35bea037193c10e2d77892cd9ff6
sherif.abdelbast@gmail.com <sherifabdelbast>
2025-04-02 00:00:52 +0200

    testing watch list with mock-data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent  } from './footer/footer.component';
+import { WatchListComponent} from './watch-list/watch-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent],
+  imports: [FooterComponent , RouterOutlet, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
