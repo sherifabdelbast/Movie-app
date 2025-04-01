9335860c2a3010664b2f8ad0c227f9edcb6bc3da
petermahfouz22 <peter.mahfouz23@gmail.com>
2025-04-01 21:35:52 +0200

    add interface for movies

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter} from '@angular/core';
 import { Router } from '@angular/router';
+import { MovieInterface } from '../models/movie.interface';
 @Component({
   selector: 'app-movie-card',
   imports: [],
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class MovieCardComponent {
 
-  @Input() movieItem : any;
+  @Input() movieItem !: MovieInterface;
   @Output() sendToParent = new EventEmitter<number>();
 
   constructor(private router: Router) {}
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -2,32 +2,60 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
 import { MovieListService } from '../services/movie-list.service';
+import { MovieInterface } from '../models/movie.interface';
 
-interface Movie {
-  id: number;
-  title: string;
-}
 @Component({
   selector: 'app-movie-list',
-  imports: [CommonModule,MovieCardComponent],
+  standalone: true,
+  imports: [CommonModule, MovieCardComponent],
   templateUrl: './movie-list.component.html',
-  styleUrl: './movie-list.component.css'
+  styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent {
-movies !: Movie[];
-moviesRequestsService = inject(MovieListService );
+  allMovies: MovieInterface[] = [];
+  movies: MovieInterface[] = [];
+  moviesRequestsService = inject(MovieListService);
 
+  ngOnInit() {
+    this.moviesRequestsService.getMoviesList().subscribe((response: any) => {
+      this.allMovies = response.results;
+      this.movies = [...this.allMovies];
+    });
+  }
+  
+  filterMovies(range: 'today' | 'week' | 'all') {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); 
 
-constructor() {}
+    switch(range) {
+      case 'today':
+        this.movies = this.allMovies.filter(movie => {
+          if (!movie.release_date) return false;
+          const releaseDate = new Date(movie.release_date);
+          return releaseDate.toDateString() === today.toDateString();
+        });
+        break;
 
-ngOnInit(){
-  this.moviesRequestsService.getMoviesList().subscribe((response : any) => {
-    this.movies = response.results;
-  });
-}
+      case 'week':
+        const endOfWeek = new Date(today);
+        endOfWeek.setDate(today.getDate() + 6);
+        endOfWeek.setHours(23, 59, 59, 999); 
+        
+        this.movies = this.allMovies.filter(movie => {
+          if (!movie.release_date) return false;
+          const releaseDate = new Date(movie.release_date);
+          return releaseDate >= today && releaseDate <= endOfWeek;
+        });
+        break;
 
-receiveFromChild(id : number) {
-console.log("PARENT COMPONENT",id);
-this.movies=this.movies.filter((movie: Movie) => movie.id !== id);
-}
+      case 'all':
+        this.movies = [...this.allMovies];
+        break;
+    }
+  }
+
+  receiveFromChild(id: number) {
+    this.allMovies = this.allMovies.filter(movie => movie.id !== id);
+    this.movies = this.movies.filter(movie => movie.id !== id);
+  }
 }
