44c38179fd44d8688b0a68d26673d3e105f614ce
sherif.abdelbast@gmail.com <sherifabdelbast>
2025-04-03 22:53:01 +0200

    fixing routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 
 import { provideHttpClient } from '@angular/common/http';
-
-
-
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
