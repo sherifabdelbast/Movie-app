37650ecce911338e90b787db1fc03ff839592a50
petermahfouz22 <peter.mahfouz23@gmail.com>
2025-04-05 20:57:11 +0200

    add favorites function and watchlist function adn fix navigate to view all movies link

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
 // navbar.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SearchComponent } from '../search/search.component';
+import { WatchlistService } from '../services/watchlist.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,8 +12,17 @@ import { SearchComponent } from '../search/search.component';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   mobileMenuOpen = false;
+  favoritesCount: number = 0;
+  
+  constructor(private watchlistService: WatchlistService) {}
+  
+  ngOnInit(): void {
+    this.watchlistService.favoritesCount$.subscribe(count => {
+      this.favoritesCount = count;
+    });
+  }
   
   toggleMobileMenu() {
     this.mobileMenuOpen = !this.mobileMenuOpen;
